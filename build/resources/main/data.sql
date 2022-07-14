82f1c5e3ad52f026b3539934e49af6523cd59d7a
Tiago Bitencourt Marques <tiagobm564@gmail.com>
2022-07-13 22:36:49 -0300

    Removed unused files

--- build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO movie(premium_year, title, studio, producer, winner)
-SELECT years,title,studios,producers,case when winner = 'yes' then true else false end as winners
-FROM CSVREAD('classpath:movielist.csv', null, 'fieldSeparator=;');
